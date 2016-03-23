df34e6921606cb73c38720e3ec04801519176f1b
James Volmert <james.volmert@gmail.com>
2016-03-23 10:29:30 -0600

    working on some more scrollmagic

---
+++ js/scrollTween.js
@@ -0,0 +1,11 @@
+(function($){
+    var wh = windown.innerHeight,
+        innerS1 = $('.innerS1'),
+        innerS2 = $('.innerS2'),
+        innerS3 = $('.innserS3'),
+        innerS4 = $('.innerS4'),
+        $screenHat = $('.screenHat'),
+        $screenA = $('.screenA'),
+        $screenB = $('.screenB'),
+        $screenC = $('.screenC');
+})(jQuery);
